a6d1d607510aaf2e3d36cc9282b7b02e1b9a17a1
Avinash Mane <avinash.mane@in.ibm.com>
2022-08-18 14:39:34 +0530

    Linkedin beta

--- lib/socialmediasite.py
+++ lib/socialmediasite.py
@@ -1,11 +1,25 @@
+###
+# Module of all web elements
+#
+#
+# structure:  page->feed->post->post components
+#
+
 import yaml
 import nerodia
+from nerodia.wait.wait import TimeoutError, Wait
+from nerodia.exception import Error, NoMatchingWindowFoundException, ObjectDisabledException, \
+    ObjectReadOnlyException, UnknownFrameException, UnknownObjectException
+# import KEYS
+from selenium.webdriver.common.keys import Keys
 import re
 import os
 import time
 import utils
 import pandas as pd
 import logging
+import sys
+sys.tracebacklimit = 3
 # if os.environ['AUTH']:
 #   AUTH=os.environ['AUTH']
 # else:
@@ -13,7 +27,12 @@ import logging
 # if os.environ['LIB']:
 #   LIB=os.environ['LIB']
 # else:
-#   LIB='./lib'  
+#   LIB='./lib' 
+import nerodia
+# import KEYS
+from selenium.webdriver.common.keys import Keys
+
+ifExists=lambda x:x.exists
 
 class SocialMediaSite:
     "Generic website"
@@ -29,7 +48,7 @@ class SocialMediaSite:
             # The FullLoader parameter handles the conversion from YAML
             # scalar values to Python the dictionary format
             auth = yaml.safe_load(file)
-            self.auth={x:auth[x] for x in auth if x in ['strava','facebook']}
+            self.auth={x:auth[x] for x in auth if x in ['strava','facebook','linkedin']}
         
     def login():
       raise NotImplemented
@@ -70,6 +89,13 @@ class SocialMediaSite:
           print(' '*level,c.tag_name,c.id,c.text.split("\n")[0][:50])
           try:SocialMediaSite._dumptag(c,level+1)
           except: pass
+
+    def scrolldown(self,pages):
+      for i in range(pages):
+        self.browser.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        # b.send_keys(Keys.CONTROL  + Keys.END )     
+        # b.driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+        time.sleep(1)
     
 Web=SocialMediaSite
 
@@ -205,48 +231,127 @@ class Strava(SocialMediaSite):
           postButton=footer.button(data_testid='post-comment-btn').wait_until(timeout=.1,method=lambda x:x.exists)
           postButton.execute_script("arguments[0].click();", postButton)
       except: pass
-    def scrolldown(self,pages):
-      for i in range(pages):
-        self.browser.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        # b.send_keys(Keys.CONTROL  + Keys.END )     
-        # b.driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
-        time.sleep(1)
+
     def goto(self,url):
       return self.browser.goto(url)
     def image(self):
-      b.execute_script("window.scrollTo(0,0)")
-      return Image(b.screenshot.png())
-    
-    def giveKudosOld(self):
-      "Give Kudos in current screen"
-      entryTag='^EntryHeader'
-      _=self.browser.buttons(title=self.giveKudosPattern)
-      for i,x in enumerate(_):
-        try:
-          self.browser.execute_script("arguments[0].click();", x)
-          card=strava.getParent(x,'div',entryTag)
-          athId,_=SocialMediaSite.getLinkByClassPatt(card,"^Avatar--")
-          actId,_=SocialMediaSite.getLinkByClassPatt(card,"^ActivityEntryBody--")
-          hdr= card.div(class_name=re.compile("^MediaBody--media-body--"))
-          try:
-            hdr.wait_until(timeout=.1,method=lambda x:x.exists)
-            name=hdr.link().text
-            loc=hdr.div(class_name=re.compile('^LocationAndTime--location--')).text
-            try:
-              id = int(re.findall(".*\/([0-9]*)",athId)[0])
-              if id not in StravaMembers:
-                id='nonMember'
-            except:
-              id='not valid'
-          except:
-            id=f'{entryTag} not found'
-        except Exception as e: 
-          logging.warning(f"giveKudos: {i} error clicking {e!r}")
+      self.browser=self.browser
+      self.browser.execute_script("window.scrollTo(0,0)")
+      return Image(self.browser.screenshot.png())
+
+class Linkedin(SocialMediaSite):
+    authType='linkedin'
+    logGSheet=None
+    def __init__(self,b=None,cookieFile=None,authFile=f"{os.environ['AUTH']}/auth.yaml",cfgFile=None):
+        super().__init__(b,authFile=authFile)
+        self.siteType='linkedin'
+        
+        if  cfgFile is not None: #=f'{os.environ["LIB"]}/cfg_strava.yaml'
+          with open(cfgFile) as file:
+            cfg = yaml.safe_load(file)
+            self.cfg.update(cfg['linkedin'])
+
+    def login(self,login):
+        if 'username' in login:
+          auth=login
         else:
-            ss.append_table([[pd.Timestamp.now().isoformat(),"INFO","giveKudos",id,athId,name,actId,loc,]])    
-            logging.info(f"giveKudos: {i},{id},{athId} {actId} {hdr.text.split(',')[:1]}")
+          auth=self.auth[self.authType][login]
+          
+        self.browser.goto('https://www.linkedin.com/login')
 
-        time.sleep(.5)
-      return 
+        # try: # reject cookies ! copy from Strava       
+      
+        try:
+          try:self.browser.text_field(name='session_key').value=auth['username']  #timeout did not work
+          except:pass
+          _pwd=self.browser.text_field(name='session_password')
+          _pwd.value=auth['password']
+          _pwd.send_keys(Keys.ENTER)
+        except Exception as e:
+          if 'dashboard' in self.browser.url:
+            logging.info( f"Logged in with {auth['username']}") #logging.warning 
+          else:
+            logging.warning( f"Error Login2 {e!r}") #logging.warning 
     
+    def logout(self):
+      try:
+        self.browser.goto('https://www.linkedin.com/feed')
+        menu=self.browser.element(class_name='global-nav__me'
+                                 ).wait_until(timeout=2,method=ifExists)
+        menu.click()
+        self.browser.execute_script("arguments[0].click();", menu)
+        if 'artdeco-dropdown--is-open' in menu.class_name:
+            self.browser.element(text='Sign Out').click()
+            self.browser.element(text='Sign Out').click()
+            logging.info( f"Logged out")
+        else:
+            logging.info( f"Error: Log out menu did not open")
+      except TimeoutError:
+            logging.info( f"Error: Perhap not signed in")
 
+    def getPostData(self,postTag):
+      "Return data regarding one post in linkedin feed"
+      post={}
+      if not postTag.exists:
+        return
+      _map={
+          'post':'postTag.parent().element(class_name="visually-hidden").text',
+          'urn':'postTag.attributes["data_urn"]',
+          'name':'postTag.element(class_name="feed-shared-actor__name").text',
+          'userUrl':'postTag.link(class_name="feed-shared-actor__container-link").href.split("?")[0]',
+          'likes':'postTag.element(class_name="social-details-social-counts__reactions-count").text',
+            # len('.div(class_name='feed-shared-social-actions').buttons(text='Like'))',
+              # 'react-button--active' in 'react-button__text--like' in 
+          'liked':'"react-button--active" in postTag.div(class_name="feed-shared-social-actions").button(text="Like").class_name',
+          # print(name','.element(class_name='feed-shared-social-actions'
+          #                                                            ).button(text='Like').class_name)
+          'text':'postTag.element(class_name="feed-shared-update-v2__commentary").text',
+          'desc':'postTag.element(class_name="feed-shared-actor__description").text',
+          'subdesc':'postTag.element(class_name="feed-shared-actor__sub-description").text',
+          # if post['subdesc'] not in ['Promoted']:
+          'deg':'postTag.span(class_name="feed-shared-actor__supplementary-actor-info").text',
+      }
+      for key,evl in _map.items():
+        try:
+            print (evl.split(').')[0]+').exists')
+            # post[key]=eval(evl,{"postTag":postTag})
+        except (nerodia.exception.UnknownObjectException,Exception) as e:
+            pass
+            print(f"Error {e!r} {key} {evl}")
+      return post    
+
+        
+    def getPostIds(self):
+      "Get list of all feed posts"
+      nerodia.default_timeout=.1
+      posts=[]
+
+      for x in self.browser.elements(class_name='feed-shared-update-v2',
+                          ):#data_id=re.compile("urn:li:activity:")):
+          if re.match(r"^urn:li:activity:",x.data_urn):
+            print (x.exists)
+            try:
+                post=self.getPostData(x)
+                post.update({'el':x})
+                posts.append( post)
+            except Exception as e:
+                # Logs the error appropriately. 
+                print(f">>{e!r}")
+          else:
+            print(">",x.data_urn)
+
+      return posts
+      
+    def deletePost(self,id):
+        "@id like post-19361836"
+        self.browser.li(id=id).div(class_name='drop-down-menu').click()
+        self.browser.li(id=id).button(text='Delete').click()
+        self.browser.alert.ok()
+
+    def deleteCurrentPost(self):
+        "delete current post"
+        self.browser.div(id="options-menu").click()
+        self.browser.div(id="options-menu").link(text="Edit").click()
+
+        self.browser.button(text='Delete').click()
+        self.browser.alert.ok()
